2eedcf4379b343f5a208c58a82706db3972ca772
= <ayxect34525599@gmail.com>
2020-12-29 11:19:59 +0900

    1st Connect to MySQL

--- app/Http/Controllers/InfectedPopulationsController.php
+++ app/Http/Controllers/InfectedPopulationsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class InfectedInTokyosController extends Controller
+class InfectedPopulationsController extends Controller
 {
   public function test(){
     return view("test");
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+// 使用するコントローラーやモデルのパスを入力
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\InfectedInTokyosController;
+use App\Http\Controllers\InfectedPopulationsController;
+use App\Models\Prefecture;
 
 /*
  * --------------------------------------------------------------------------
@@ -11,11 +13,18 @@ use App\Http\Controllers\InfectedInTokyosController;
  * Laravel 8以降では、ルーティングの使用が変更されている
  * 1. 上部にコントローラーのパスを入力すること
  * 2. Route::get('パス', [コントローラー名::class, 'コントローラー内の関数名']); という表記に従うこと
- *  * getの部分はpostに変更してもよい
+ *  * getの部分はpostなどに変更してもよい
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', [InfectedInTokyosController::class, 'test']);
+Route::get('/tokyo', function(){
+  $prefectures = Prefecture::all();
+  foreach($prefectures as $prefecture) {
+    return $prefecture->name;
+  }
+});
+
+Route::get('/test', [InfectedPopulationsController::class, 'test']);
